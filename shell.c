3f7e7c6c9dc5468218bdd52e2d8605373335e19c
Divya-Prerna <31502977+Divya-Prerna@users.noreply.github.com>
2017-11-05 18:28:10 +0530

    Add files via upload

---
+++ shell.c
@@ -0,0 +1,47 @@
+#include  <stdio.h>
+#define size 7
+ 
+
+int shell_sort(int []);
+ 
+void main()
+{
+    int arr[size], i;
+    printf("Enter the elements to be sorted:");
+    for (i = 0;i < size;i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    shell_sort(arr);
+    printf("The array after sorting is:");
+    for (i = 0;i < size;i++)
+    {
+        printf("\n%d", arr[i]);
+    }
+}
+ 
+
+int shell_sort(int array[])
+{
+    int i = 0, j = 0, k = 0, mid = 0;
+    for (k = size / 2;k > 0;k /= 2)
+    {
+        for (j = k;j < size;j++)
+        {
+            for (i = j - k;i >= 0;i -= k)
+            {
+                if (array[i + k] >= array[i])
+                {
+                    break;
+                }
+                else
+                {
+                    mid = array[i];
+                    array[i] = array[i + k];
+                    array[i + k] = mid;
+                }
+            }
+        }
+    }
+    return 0;
+}
